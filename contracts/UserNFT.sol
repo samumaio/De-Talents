6f9f370fb264473deef6aae0f9d73c771622df21
samumaio <samuelemaiorino10@gmail.com>
2025-02-26 18:58:01 +0100

    USERNFT contract is created

---
+++ contracts/UserNFT.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+/// @title User's ERC 721 NFT
+/// @author samumaio
+/// @notice This smart contract mints a personal NFT on behalf of the sender of this contract. Furthermore this contract should be a Soul bound token so as to not be transfered
+
+contract UserNFT is ERC721 {
+    uint256 public tokenCounter;
+    uint256 public baseFee;
+    address payable private owner;
+    mapping(address => bool) private institutions;
+    mapping(uint256 => string) private tokenURIs;
+    //modifiers
+    /// @notice Only verified istiutitons enabled
+    // modifier onlyIstitutions() {
+    //     require(institutions[msg.sender], notAnInstitution());
+    //     _;
+    // }
+    modifier onlyOwner() {
+        require(msg.sender == owner, notAllowed());
+        _;
+    }
+    //events
+    event mintedUserNFT(address recipient);
+    //errors
+    error notEnoughETHProvided();
+    error withdrawFailed();
+    error notAllowed();
+
+    constructor(uint256 fee) ERC721("USERNFT", "UFT") {
+        tokenCounter = 0;
+        baseFee = fee;
+        owner = payable(msg.sender);
+    }
+
+    function mintNFT(
+        address recipient,
+        string memory tokenURI
+    ) public payable returns (uint256) {
+        require(msg.value >= baseFee, notEnoughETHProvided());
+        _mint(recipient, tokenCounter);
+        //Sets the tokenURI for the newly minted NFT
+        tokenURIs[tokenCounter] = tokenURI;
+        tokenCounter++;
+        return tokenCounter - 1;
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success, withdrawFailed());
+    }
+
+    function isCertified(address institution) public view returns (bool) {
+        return institutions[institution];
+    }
+
+    function getBaseFee() public view returns (uint256) {
+        return baseFee;
+    }
+}
